13734800b7feba06ad0eaa89e556be4ac5841cfa
Patrick Veverka <veverkap@users.noreply.github.com>
2023-01-02 01:14:17 -0500

    wonder

--- unique_id.rb
+++ unique_id.rb
@@ -2,19 +2,49 @@ require 'securerandom'
 
 module Rack
   class RequestId
-    def initialize(app, opts = {})
+    REQUEST_ID = "HTTP_X_REQUEST_ID"
+
+    def self.current
+      Thread.current.thread_variable_get(:request_id)
+    end
+
+    # Gets the request ID from the environment.
+    #
+    # env - The request Environment.
+    #
+    # Returns the ID String.
+    def self.get(env)
+      env[REQUEST_ID]
+    end
+
+    def initialize(app)
       @app = app
-      @storage = opts[:storage] || proc { Thread.current }
-      @id_generator = opts[:id_generator] || proc { SecureRandom.uuid }
     end
 
     def call(env)
-      storage = @storage.respond_to?(:call) ? @storage.call : @storage
-      storage[:request_id] = env['HTTP_X_REQUEST_ID'] || @id_generator.call
-      status, headers, body = @app.call(env)
+      began_at = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
 
-      headers['X-Request-Id'] ||= storage[:request_id]
+      request_id = external_request_id(env) || internal_request_id
+      env[REQUEST_ID] = request_id
+      Thread.current.thread_variable_set(:request_id, request_id)
+
+      status, headers, body = @app.call(env)
+      finished_at = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+      elapsed = finished_at - began_at
+
+      headers["X-Request-Id"] = request_id
+      headers["Server-Timing"] = "app;dur=#{elapsed}"
       [status, headers, body]
+    end
+
+    private
+
+    def external_request_id(env)
+      request_id = env[REQUEST_ID]
+    end
+
+    def internal_request_id
+      SecureRandom.uuid
     end
   end
 end
--- webhook.rb
+++ webhook.rb
@@ -5,19 +5,37 @@ require_relative "unique_id"
 use Rack::RequestId
 
 get "/" do
-  json message: "Hello World"
+  json request.env.sort.to_h
+end
+
+post "/:service/" do
+  status, headers, body = call env.merge("PATH_INFO" => '/', "SERVICE" => params[:service])
+  [status, headers, body]
 end
 
 post "/" do
-  request_id = Thread.current[:request_id].to_s
+  puts request.env["SERVICE"]
+  request_id = Rack::RequestId.current
 
   request.body.rewind  # in case someone already read it
-  data = JSON.parse request.body.read
+  data = request.body.read
 
-  # write data to file
-  File.open("requests/#{request_id}.json", 'w') do |f|
-    f.write(JSON.pretty_generate(data))
+  if request.accept == "application/json" || request.env["CONTENT_TYPE"] == "application/json"
+    data = JSON.parse(data)
+
+    # write data to file
+    File.open("requests/#{request_id}.json", 'w') do |f|
+      f.write(JSON.pretty_generate(data))
+    end
   end
+  response = {
+    accept: request.accept,
+    env: environment(request.env),
+    body: data
+  }
+  json response
+end
 
-  json data
+def environment(env)
+  env.reject { |k, v| k.start_with?("rack.") || k.start_with?("sinatra.") || k.start_with?("puma.") }.sort.to_h
 end
